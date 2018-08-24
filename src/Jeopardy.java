68148d1412633a00f640795ad52568340a21313e
league <league@wts22.attlocal.net>
2018-08-23 19:56:36 -0700

    Jeopardy

--- src/Jeopardy.java
+++ src/Jeopardy.java
@@ -8,9 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -20,8 +17,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
-import java.io.File;
 
 /* This recipe is to be used with the Jeopardy Handout: http://bit.ly/1bvnvd4 */
 
@@ -58,10 +53,10 @@ public class Jeopardy implements ActionListener {
 		firstButton = createButton("100");
 		// 7. Add the firstButton to the quizPanel
 		quizPanel.add(firstButton);
-	
+
 		// 8. Write the code inside the createButton() method below. Check that your
 		// game looks like Figure 1 in the Jeopardy Handout - http://bit.ly/1bvnvd4.
-		
+
 		// 9. Use the secondButton variable to hold a button using the createButton
 		// method
 		secondButton = createButton("dollarAmount");
@@ -97,32 +92,33 @@ public class Jeopardy implements ActionListener {
 		// Increment the buttonCount (this should make the layout vertical)
 		buttonCount++;
 		// Return your new button instead of the temporary button
-		
+
 		return button;
 	}
 
 	public void actionPerformed(ActionEvent arg0) {
 		// Remove this temporary message:
-		//JOptionPane.showMessageDialog(null, "pressed " + ((JButton) arg0.getSource()).getText() + " button");
+		// JOptionPane.showMessageDialog(null, "pressed " + ((JButton)
+		// arg0.getSource()).getText() + " button");
 
 		// Use the method that plays the jeopardy theme music.
 		playJeopardyTheme();
 		JButton buttonPressed = (JButton) arg0.getSource();
 		// If the buttonPressed was the firstButton
-		if(buttonPressed == firstButton) {
+		if (buttonPressed == firstButton) {
 			askQuestion("How heavy is the largest pizza on earth in pounds", "26883 pounds", 100);
 		}
-		
-		//Call the askQuestion() method
+
+		// Call the askQuestion() method
 
 		// Fill in the askQuestion() method. When you play the game, the score should
 		// change.
 
 		// Or if the buttonPressed was the secondButton
-		if(buttonPressed == secondButton) {
+		if (buttonPressed == secondButton) {
 			askQuestion("How many feet is the longest pizza in the world", "6330 feet", 200);
 		}
-		
+
 		// Call the askQuestion() method with a harder question
 
 		// Clear the button text (set the button text to nothing)
@@ -132,25 +128,29 @@ public class Jeopardy implements ActionListener {
 	private void askQuestion(String question, String correctAnswer, int prizeMoney) {
 		// Remove this temporary message
 		JOptionPane.showMessageDialog(null, "this is where the question will be asked");
-		
+
 		// Use a pop up to ask the user the question
 		String bob = JOptionPane.showInputDialog(question);
 		// If the answer is correct
-		if(bob.equals(correctAnswer)) {
+		if (bob.equals(correctAnswer)) {
 			score += prizeMoney;
 			updateScore();
 			JOptionPane.showMessageDialog(null, "You are correct");
-		}
-		else {
-			score-= prizeMoney;
+		} else {
+			score -= prizeMoney;
 			JOptionPane.showMessageDialog(null, "The correct answer is 26883 pounds");
 		}
 		// Increase the score by the prizeMoney
-		
+		score += prizeMoney;
 		// Call the updateScore() method
-		
+		updateScore();
 		// Pop up a message to tell the user they were correct
-		
+		if (bob.equals(correctAnswer)) {
+			JOptionPane.showMessageDialog(null, "You are correct");
+		} else {
+			score -= prizeMoney;
+			JOptionPane.showMessageDialog(null, correctAnswer);
+		}
 		// Otherwise
 
 		// Decrement the score by the prizeMoney
