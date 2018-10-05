4110e44ed17b7db025c8816ab407fd3f8e291669
league <league@wts20.attlocal.net>
2018-10-04 19:53:52 -0700

    Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,6 +5,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Calculator {
+	
 JFrame frame = new JFrame();
 JPanel panel = new JPanel();
 JLabel label = new JLabel();
@@ -14,5 +15,21 @@ JButton button3 = new JButton();
 JButton button4 = new JButton();
 JTextField textfield = new JTextField();
 JTextField textfields = new JTextField();
+public static void main(String[] args) {
+	
+}
+Calculator(){
+	
+	frame.add(panel);
+	panel.add(label);
+	panel.add(button1);
+	panel.add(button2);
+	panel.add(button3);
+	panel.add(button4);
+	panel.add(textfield);
+	panel.add(textfields);
+	
+	frame.pack();
+}
 
 }
