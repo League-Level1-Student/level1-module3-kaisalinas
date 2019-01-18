87c372d3f218bea768b1d2d81f693d144030e2e0
league <league@wts8.attlocal.net>
2019-01-17 19:46:18 -0800

    Gridworld

--- src/extra/Gridworld.java
+++ src/extra/Gridworld.java
@@ -12,10 +12,15 @@ import info.gridworld.world.World;
 
 public class Gridworld {
 	World world = new World();
-	Bug bug1 = new Bug();
+	Bug bug1 = new Bug(Color.BLUE);
 	Bug bug2 = new Bug();
-	Bug bug3 = new Bug(Color.BLUE);
+	Bug bug3 = new Bug();
 	Flower flower = new Flower();
+	Flower flower2 = new Flower();
+	Flower flower3 = new Flower();
+	static Random factory = new Random();
+	int row;
+	int col;
 	
 	public static void main(String[] args) {
 		Gridworld gridworld = new Gridworld();
@@ -23,16 +28,25 @@ public class Gridworld {
 		
 	}
 		public void drawWorld() {
-			world.show();	
-			Location bugPlace = new Location(factory.nextInt(10),factory.nextInt(10));	
+			world.show();
+			row = factory.nextInt(10);
+			col = factory.nextInt(10);
+			Location bugPlace = new Location(row, col);	
 			Location place = new Location(100, 100);
 			world.add(bugPlace, bug1);
 			bug1.turn();
-		world.add(bugPlace, flower);
+			
+		Location flowerPlace = new Location(row, col-1);
+		world.add(flowerPlace, flower);
+		Location flowerPlace2 = new Location(row, col+1);
+		world.add(flowerPlace2, flower2);
+	
+		for(int col=0; col<10; col++) {
+		for( row=0; row<10; row++) {
+			Location flowerPlace3 = new Location(row, col);
+			world.add(flowerPlace3, flower3);
+			}
 		}
-	
+	}
 		
-	static Random factory = new Random();
-	
-	
 }
